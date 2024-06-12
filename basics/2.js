97d40787791ed56f2f8da3014f0f567a7482b012
Priya20Vandana <122594674+Priya20Vandana@users.noreply.github.com>
2024-06-12 15:00:15 +0000

    container config done

---
+++ basics/2.js
@@ -0,0 +1,16 @@
+const accountId = 75298352
+let accountEmail = "yhrw@gmail.com"
+var accountPassword = "7843"
+ accountCity = "Bihar"
+/*
+Prefer not to use var and without any data type
+preferably use let and const.
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
+accountEmail = "yh854hrw@gmail.com"
+ accountPassword = "788946843"
+ accountCity = "Udaipur"
+
+ console.table([accountId, accountEmail, accountPassword, accountCity])
