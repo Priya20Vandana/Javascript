7e2162af6ebbbab7b5109fba79d1251076fe3855
Priya20Vandana <122594674+Priya20Vandana@users.noreply.github.com>
2024-06-14 16:54:32 +0000

    datatype done

---
+++ basics/3.js
@@ -0,0 +1,45 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // will give error cause we are using nodejs, not browser
+
+console.log(3+3); console.log("pv"); // do not write like this cause of code readability
+//OR
+console.log(3+3)
+console.log("pv")    //prefer to write like this cause of code readability
+  
+
+//datatypes
+
+let name = "priya vandana"
+let age = 19
+let isLoggedIn = false
+
+//number-> 2 to the power 53
+//bigint-> bigger than number
+//string->""
+//Boolean-> true/false
+//null-> standalone value
+//undefined-> undefined
+//symbol-> unique
+//object-> see furthur
+
+
+console.log(typeof undefined); //undefined
+console.log(typeof null);//object
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
