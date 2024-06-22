623a112a7497dc4c64ad5ec60d30ac426a97d8c4
Priya20Vandana <122594674+Priya20Vandana@users.noreply.github.com>
2024-06-22 15:59:01 +0000

    operations

---
+++ basics/5.js
@@ -0,0 +1,42 @@
+let value = 1
+let negValue = -value
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**4);
+console.log(5/2);
+console.log(5%2);
+
+let str1 = "priya"
+let str2 = " vandana"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1"+ 2);
+console.log(1+ "2");
+console.log("1"+ 2 + 2);
+console.log(1+ 2 + "2");
+
+console.log(3+4*6%6) // not used preferably in coding
+
+
+console.log(+true); //1
+console.log(+"") //0
+
+let num1, num2, num3
+num1 = num2 = numn3 = 87*09
+// why to use like this if same value is assigned in every variale. not feasible
+
+
+let gameCounter = 100
+gameCounter++ ;
+console.log(gameCounter);
+++gameCounter;
+console.log(gameCounter);
+
+
+
+
+
