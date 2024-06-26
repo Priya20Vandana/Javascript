35a1d7268af55f987303acb921ad837021ab922b
Priya20Vandana <122594674+Priya20Vandana@users.noreply.github.com>
2024-06-26 16:01:55 +0000

    comparison done

---
+++ basics/6.js
@@ -0,0 +1,26 @@
+//console.log(2>1);
+//console.log(2>=1);
+//console.log(2<1);
+//console.log(2>=1);
+//console.log(2==1);
+//console.log(2!=1);
+
+
+
+console.log("2">1);
+console.log("02">1);
+
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+
+console.log(undefined>0);
+console.log(undefined==0);
+console.log(undefined<0);
+// above (till 14) are confusing codes so better avoid.
+
+// === strict check
+
+console.log("2"===2);
